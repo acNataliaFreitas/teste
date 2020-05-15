8b13fe7aa9045bf6b39c5d9d486c80f366a8dcf1
Natália Freitas <natyff1@hotmail.com>
2020-05-15 10:28:40 -0300

    leitura de dados

--- src/teste/teste1.java
+++ src/teste/teste1.java
@@ -1,5 +1,16 @@
 package teste;
 
-public class teste1 {
+import java.util.Locale;
+import java.util.Scanner;
 
+public class teste1 {
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		System.out.println("Bom Dia");
+}
 }
